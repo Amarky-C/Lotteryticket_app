1a40f8288bcf5afe996e0cd73e8787b78739f59d
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-03-30 14:29:18 +0100

    Amaka's lottery ticket application

---
+++ lottery.py
@@ -0,0 +1,18 @@
+#enter the players name
+#enter the number
+#random randit within an inclusive range
+#
+import random
+def lottery_calc():
+    lottery_player = input('Enter players name: ')
+    lottery_number = int(input('Enter any number from 0-50: '))
+    lottery_gen = random.randint(0,50)
+    if lottery_gen == lottery_number:
+        print(lottery_player, 'congratulations', lottery_number, 'is the lottery winning number')
+    else:
+        print(lottery_player, 'sorry you lost try again', lottery_number, 'is not the lottery winning number')
+
+
+
+
+lottery_calc()
