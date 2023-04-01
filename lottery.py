6f6ace735d0c83fbce0aec805249fbae804d598c
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-04-01 20:58:33 +0100

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -7,15 +7,16 @@
 #the user wins the lottery
 #if the number doesn't match, the user loses and will try again
 
+winning_number = 30
 import random
 def lottery_calc():
     lottery_player = input('Enter players name: ')
     lottery_number = int(input('Enter any number from 0-50: '))
     lottery_gen = random.randint(0,50)
-    if lottery_gen == lottery_number:
-        print(lottery_player, 'congratulations', lottery_number, 'is the lottery winning number')
+    if lottery_gen == winning_number and lottery_number = lottery_gen and winning_number:
+        print(lottery_player, 'congratulations', lottery_gen, 'is the winning_number', winning_number, 'is the lottery winning number')
     else:
-        print(lottery_player, 'sorry you lost try again', lottery_number, 'is not the lottery winning number')
+        print(lottery_player, 'sorry you lost try again', lottery_gen, 'is not the winning number', winning_number, 'is the lottery winning number')
 
 
 
